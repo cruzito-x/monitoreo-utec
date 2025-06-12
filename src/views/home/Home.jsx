3de6407be2854bc6941f54b31a7da2598a3ba532
David Cruz <dcruzer92@gmail.com>
2025-06-12 05:43:54 -0600

    Parking position fixed

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -8,10 +8,15 @@ const Home = () => {
     <div className="min-h-screen flex flex-col">
       <Top />
 
-      <main className="flex-1 px-10 py-8">
-        <Cards />
-        {/* <Parking /> */}
+      <main className="flex-1 px-4 sm:px-6 lg:px-10 py-8">
+        <div className="flex flex-col lg:flex-row gap-6">
+          <Cards />
+          <div className="flex">
+            <Parking />
+          </div>
+        </div>
       </main>
+
       <Bottom />
     </div>
   );
